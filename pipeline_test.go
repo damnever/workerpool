daa93488e8d246337376b3ebcf0c784bd0a065fa
Xiaochao Dong (@damnever) <the.xcdong@gmail.com>
2023-04-06 19:35:47 +0800

    Pipeline made easy
    Signed-off-by: Xiaochao Dong (@damnever) <the.xcdong@gmail.com>

---
+++ pipeline_test.go
@@ -0,0 +1,50 @@
+package workerpool
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPipeline_AsyncExecutor(t *testing.T) {
+	t.Parallel()
+
+	makeExecutor := func() (*atomic.Int32, AsyncExecutor) {
+		counter := &atomic.Int32{}
+		return counter, func(ctx context.Context, fn Func) error {
+			counter.Add(1)
+			go fn(ctx)
+			return nil
+		}
+	}
+	feederCount, feederExecutor := makeExecutor()
+	workerCount, workerExecutor := makeExecutor()
+
+	pipeline := NewPipelineWith[int, int](PipelineOptions{
+		FeederAsyncExecutor: feederExecutor,
+		WorkerAsyncExecutor: workerExecutor,
+	})
+
+	_ = pipeline.StartFeeder(context.Background(), []int{1, 2})
+	_ = pipeline.StartFeeder(context.Background(), []int{3, 4})
+	_ = pipeline.StartWorker(context.Background(), func(_ context.Context, i int) int {
+		return i * 2
+	})
+	_ = pipeline.StartWorker(context.Background(), func(_ context.Context, i int) int {
+		return i * 2
+	})
+	_ = pipeline.StartWorkerN(context.Background(), 2, func(_ context.Context, i int) int {
+		return i * 2
+	})
+
+	require.Equal(t, int32(2), feederCount.Load())
+	require.Equal(t, int32(4), workerCount.Load())
+	sum := 0
+	for v := range pipeline.Join() {
+		sum += v
+	}
+	require.Equal(t, 20, sum)
+	require.Equal(t, 4, pipeline.ProcessedCount())
+}
